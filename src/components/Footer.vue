3ce53dec836fe26edfe8fbc95969c1a4adb700e2
Rodkross <pretorsouza@gmail.com>
2023-07-26 21:32:53 -0300

    finalizando footer e cabecalho

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-    <p>Footer</p>
+    <div id="footer">
+        <p>Make Your Burguer &copy; 2023</p>
+    </div>
 </template>
 
 <script>
@@ -8,3 +10,17 @@ export default {
 }
 
 </script>
+
+<style scoped>
+#footer{
+    height: 200px;
+    background-color: #222;
+    border-top: 4px solid #111;
+    color: #fcba03;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    bottom: 0;
+    width: 100%;
+}
+</style>
